01a9bbdf933a8a633f115070f04a4a685cb5abc1
thatging3rkid <thatging3rkid@users.noreply.github.com>
2017-04-15 14:18:01 -0400

    controller.ino -> updated code from monday

--- rit_first_controller/rit_first_controller.ino
+++ rit_first_controller/rit_first_controller.ino
@@ -15,15 +15,16 @@ void setup() {
   if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
     Serial.println("setRF failed");   
 
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
   pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
 }
 
 void loop() {
   
-  uint8_t data[] = {(analogRead(A0) / 4), (analogRead(A1) / 4), digitalRead(2), digitalRead(3), 0, 0};
+  uint8_t data[] = {((analogRead(A0) - 1) / 4), ((analogRead(A1) - 8) / 4), digitalRead(2), digitalRead(3), digitalRead(4), digitalRead(5)};
   nrf24.send(data, sizeof(data));
 
   Serial.print("Sending: ");
@@ -32,7 +33,39 @@ void loop() {
   Serial.print(data[1]);
   Serial.print(" ");
   Serial.print(data[2]);
+  Serial.print(" ");
+  Serial.print(data[3]);
+  Serial.print(" ");
+  Serial.print(data[4]);
+  Serial.print(" ");
+  Serial.print(data[5]);
+  Serial.print(" ");
+  Serial.print(data[6]);
+  Serial.print(" ");
   Serial.print("\n");
+  
 
+  nrf24.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (nrf24.waitAvailableTimeout(100))
+  { 
+    // Should be a reply message for us now   
+    if (nrf24.recv(buf, &len))
+    {
+      Serial.print("got reply: ");
+      Serial.println((char*)buf);
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is nrf24_server running?");
+  }  
   delay(20);
 }
