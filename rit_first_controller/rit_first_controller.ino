af2dec64216177604b3aebe1ae4d08b557236ed1
Gregory Goh (Liang) <gohliang@gmail.com>
2017-04-15 17:01:56 -0400

    Add loop to handle address changing
    - Added comments to controller code
    - Created a function that gets called during setup; it listens for input
    to decide which address preset to use

--- rit_first_controller/rit_first_controller.ino
+++ rit_first_controller/rit_first_controller.ino
@@ -3,6 +3,12 @@
 
 RH_NRF24 nrf24;
 
+// Address presets
+const uint8_t ROBOT_0_ADDR = '0xE7E7E7F0';
+const uint8_t ROBOT_1_ADDR = '0xE7E7E7F1';
+const uint8_t ROBOT_2_ADDR = '0xE7E7E7F2';
+const uint8_t ROBOT_3_ADDR = '0xE7E7E7F3';
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) 
@@ -15,18 +21,24 @@ void setup() {
   if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
     Serial.println("setRF failed");   
 
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(A0, INPUT);
+  pinMode(2, INPUT_PULLUP);   // Left Button
+  pinMode(3, INPUT_PULLUP);   // Up Button
+  pinMode(4, INPUT_PULLUP);   // Right Button
+  pinMode(5, INPUT_PULLUP);   // Down Button
+  pinMode(A0, INPUT);         // Thumbstick Y (front/back)
+  pinMode(A1, INPUT);         // Thumbstick X (left/right)
+
+  // Loop for the specified milliseconds, waiting for address preset input
+  addressPresetLoop(5000);
 }
 
 void loop() {
-  
-  uint8_t data[] = {((analogRead(A0) - 1) / 4), ((analogRead(A1) - 8) / 4), digitalRead(2), digitalRead(3), digitalRead(4), digitalRead(5)};
-  nrf24.send(data, sizeof(data));
 
+  // read input and build data array
+  uint8_t data[] = {((analogRead(A0) - 1) / 4), ((analogRead(A1) - 8) / 4), digitalRead(2), digitalRead(3), digitalRead(4), digitalRead(5)};
+  nrf24.send(data, sizeof(data));   // send data to the network address
+
+  // Write data to serial
   Serial.print("Sending: ");
   Serial.print(data[0]);
   Serial.print(" ");
@@ -69,3 +81,30 @@ void loop() {
   }  
   delay(20);
 }
+
+/*****************************************************************
+Description:
+This method starts a loop that listens for button input that
+decides what network address preset to use. 
+Uses default network address upon timeout.
+ 
+Parameters:
+  interval -> the time in milliseconds to loop and wait for input
+
+Author: Gregory Goh
+
+******************************************************************/
+void addressPresetLoop (unsigned long interval) {
+  unsigned long previousTime = millis();  // start a timer
+
+  // keep listening for input to decide which address preset to use
+  while ((millis() - previousTime) < interval) {
+    if (digitalRead(2)) nrf24.setNetworkAddress(&ROBOT_0_ADDR, 4);
+    if (digitalRead(3)) nrf24.setNetworkAddress(&ROBOT_1_ADDR ,4);
+    if (digitalRead(4)) nrf24.setNetworkAddress(&ROBOT_2_ADDR ,4);
+    if (digitalRead(5)) nrf24.setNetworkAddress(&ROBOT_3_ADDR ,4);
+  }
+
+  // network address defaults to 0xE7E7E7E7E7 after timeout
+}
+
